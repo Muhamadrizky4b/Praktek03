11faa37793f9067b2807ef811644d245621a2e47
Muhamad Rizky <rzky033@gmail.com>
2023-05-28 13:54:51 +0800

    attribute dan method

--- src/praktek03/SepedaMotor.java
+++ src/praktek03/SepedaMotor.java
@@ -4,5 +4,9 @@
  */
 package praktek03;
 public class SepedaMotor extends Sepeda {
-    
+    private String jenisMotor;
+   
+    void throttle() {
+        System.out.println("NGEEEEENG.........");
+    }
 }
